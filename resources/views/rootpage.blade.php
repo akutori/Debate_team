d565e69dc829f131e3938c012d22a1c8635a1dcd
宮里一樹 <2001204@s.asojuku.ac.jp>
2022-09-28 10:16:10 +0900

    管理者画面各機能に関するblade・Controllerのファイルを追加 現時点では、ページ遷移のみを実装 機能は、作成中

---
+++ resources/views/rootpage.blade.php
@@ -0,0 +1,57 @@
+@extends('header')
+
+@section('head')
+
+@endsection
+<body>
+<h1 class="text-center p-5 display-1"><strong>管理者画面</strong></h1>
+<div class="container-fluid">
+
+
+    <div class="row pb-lg-5 me-5">
+        <div class="col-9 text-end align-self-center">
+            <p class="display-3">管理者名さん</p>
+        </div>
+    </div>
+
+    <div class="row row-cols-1 row-cols-lg-2 justify-content-center">
+
+        <div class="col col-lg-auto card my-3 mx-auto mx-lg-3 shadow-lg" style="width: 17rem;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQATMbubZqJxGWjeHrRSvVyO3nFihSmi7mdOw&usqp=CAU" alt="NGの画像" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">NGワード編集</h5>
+                <p class="card-text">登録されたNGワードを確認します</p>
+                <a href="{{url('/ngwordEdit')}}" class="btn btn-primary btn-lg shadow-lg">NGワード編集</a>
+            </div>
+        </div>
+
+        <div class="col col-lg-auto card my-3 mx-auto mx-lg-3 shadow-lg" style="width: 17rem;">
+            <img src="https://1.bp.blogspot.com/-ngMRSaxyRfA/WM9XgLQPplI/AAAAAAABCrA/EC4t9JLB4gA6WSZe58KbeGLIXm4ljyRfACLcB/s800/art_live_painting.png" alt="カードの画像" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">お題作成</h5>
+                <p class="card-text">公式が考えたお題で部屋を作成します</p>
+                <a href="{{url('/addTitle')}}" class="btn btn-primary btn-lg shadow-lg">お題作成</a>
+            </div>
+        </div>
+
+        <div class="col col-lg-auto card my-3 mx-auto mx-lg-3 shadow-lg" style="width: 17rem;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-uoW4rwKly5fP9BgBi9xo-_rLZe4Dl7U2mQ&usqp=CAU" alt="ルームの画像" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">ルーム一覧</h5>
+                <p class="card-text">ディベートがされているルームを全て表示します</p>
+                <a href="{{url('roomAll')}}" class="btn btn-primary btn-lg shadow-lg">ルーム一覧</a>
+            </div>
+        </div>
+
+        <div class="col col-lg-auto card my-3 mx-auto mx-lg-3 shadow-lg" style="width: 17rem;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgztcjMq8MIAMhtZHNQ53IiijOUOjklbpP1g&usqp=CAU" alt="時計の画像" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">チャット時間の更新</h5>
+                <p class="card-text">ディベートの時間の編集をします</p>
+                <a href="{{url('timeChange')}}" class="btn btn-primary btn-lg shadow-lg">チャット時間の編集</a>
+            </div>
+        </div>
+
+    </div>
+</div>
+</body>
