3a5ec067cb95ed93db6b837d8fcaf884b276c3a5
akutori <78586241+akutori@users.noreply.github.com>
2022-10-02 12:46:17 +0900

    コードの整形と傍観者に何も表示されない問題を修正

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -23,13 +23,13 @@ use Illuminate\Support\Facades\Auth;
         <h1 class="text-center mt-5 mb-3">{{$roomdata->t_name}}</h1>
         <div class="col-2"></div>
         @if($state==0)
-            @if($usersposition="賛成")
+            @if($usersposition=="賛成")
                 <p class="fs-2 text-center col-8">あなたは<span class="fs-1 text-danger">{{$usersposition}}派</span>です</p>
-            @else
+            @elseif($usersposition=="反対")
                 <p class="fs-2 text-center col-8">あなたは<span class="fs-1 text-primary">{{$usersposition}}派</span>です</p>
             @endif
         @elseif($state==1)
-            <p>あなたの立場は傍観者です</p>
+            <p class="fs-2 text-center col-8">あなたは<span class="fs-1 text-success">傍観者</span>です</p>
         @endif
             {{--タイマー--}}
             <div class="col-auto col-lg-2 justify-content-start">
@@ -41,40 +41,41 @@ use Illuminate\Support\Facades\Auth;
                 {{-- チャット履歴を表示させる --}}
             </div>
         </div>
-@if($state==0)
-    {{---  チャット送信  ---}}
-    {{--- web.phpの/chat ---}}
-        <form action="{{url('/chat/'.$roomdata->r_id.'/'.$state)}}" method="post" id="chatform" class="mt-3">
-            <div class="row">
-            @csrf
-            <input type="hidden" name="user_id" value="{{$id = auth()->id()}}">
-            <input type="hidden" name="user_name" value="{{$name}}">
-            <input id="room_id" type="hidden" name="room_id" value="{{$roomdata->r_id}}">
-            <input type="hidden" name="users_position" value="{{$usersposition}}">
-            <div class="col-9">
-            <input
-                id="message"
-                type="text"
-                max="500"
-                required
-                name="message"
-                placeholder="メッセージを入力"
-                class="form-control py-3">
-            </div>
-            <div class="col-2">
-                <button type="submit" id="submit" class="btn btn-primary btn-lg mb-1 form-control py-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
-                    </svg>
-                </button>
-            </div>
-            </div>
-        </form>
-        </div>
-    <script src="{{ asset('js/chat.js') }}"></script>
-@else
-    <input id="room_id" type="hidden" name="room_id" value="{{$roomdata->r_id}}">
-    {{-- 傍観者の場合コチラが表示される --}}
-@endif
+    @if($state==0)
+        {{---  チャット送信  ---}}
+        {{--- web.phpの/chat ---}}
+            <form action="{{url('/chat/'.$roomdata->r_id.'/'.$state)}}" method="post" id="chatform" class="mt-3">
+                <div class="row">
+                @csrf
+                <input type="hidden" name="user_id" value="{{$id = auth()->id()}}">
+                <input type="hidden" name="user_name" value="{{$name}}">
+                <input id="room_id" type="hidden" name="room_id" value="{{$roomdata->r_id}}">
+                <input type="hidden" name="users_position" value="{{$usersposition}}">
+                <div class="col-9">
+                <input
+                    id="message"
+                    type="text"
+                    max="500"
+                    required
+                    name="message"
+                    placeholder="メッセージを入力"
+                    class="form-control py-3">
+                </div>
+                <div class="col-2">
+                    <button type="submit" id="submit" class="btn btn-primary btn-lg mb-1 form-control py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
+                        </svg>
+                    </button>
+                </div>
+                </div>
+            </form>
+
+    @else
+        <input id="room_id" type="hidden" name="room_id" value="{{$roomdata->r_id}}">
+        {{-- 傍観者の場合コチラが表示される --}}
+    @endif
+        <script src="{{ asset('js/chat.js') }}"></script>
+    </div>
 </body>
 </html>
