531b72bc100f8303ee52bdbba154e1fdbd7b137f
tsubasa <2001218@s.asojuku.ac.jp>
2022-10-05 12:45:44 +0900

    bootstrapを試みた ランキングのlistデザイン（てきとう）

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -1,36 +1,35 @@
-<html>
-<body>
-<h1>Mypage</h1>
-<p>{{$username}}さん</p>
-<button type="button" onclick="location.href='{{url('/sgenre')}}'">ジャンル選択</button>
-<button type="button" onclick="location.href='{{url('/makeroom')}}'">ルーム作成</button>
-<button type="button" onclick="location.href='{{url('/ranking')}}'">ランキングに行く</button>
-<button type="button" onclick="location.href='{{url('/delroom')}}'">ルーム削除</button>
-<button type="button" onclick="location.href='{{url('/readme')}}'">説明を見る</button>
+<html lang="ja">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <body>
 @foreach($user as $users)
     <!-- top3 -->
-    <div>
-        <p>1位</p>
-        <p>名前:{{$users[0]->name}}</p>
-        <p>ポイント：{{$users[0]->u_point}}</p>
+     <div class="list-group" style="max-width: 400px; ">
+        <a class="list-group-item">1位</a>
+        <a class="list-group-item">名前:{{$users[0]->name}}</a>
+        <a class="list-group-item">ポイント：{{$users[0]->u_point}}</a>
     </div>
     <div>
         <p>2位</p>
         <p>名前:{{$users[1]->name}}</p>
         <p>ポイント：{{$users[1]->u_point}}</p>
     </div>
-    <div>
         <p>3位</p>
         <p>名前:{{$users[2]->name}}</p>
         <p>ポイント：{{$users[2]->u_point}}</p>
     </div>
     @for($i = 3; $i <count($users); $i++)
-        {{$i+1}}位 名前：{{$users[$i]->name}}ポイント：{{$users[$i]->u_point}}<br>
+        <div class="list-group" style="max-width: 400px;">
+            <a class="list-group-item"> {{$i+1}}位 名前：{{$users[$i]->name}}  ポイント：{{$users[$i]->u_point}}</a>
+        </div>
     @endfor
 @endforeach
-
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
-
 </html>
+
+
