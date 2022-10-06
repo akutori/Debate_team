713910782a98aa318aa1fccc3d06d599e5d4dbb3
tsubasa <2001218@s.asojuku.ac.jp>
2022-10-06 18:27:00 +0900

    ランキングのデザイン(marginで１位～３位、４位以降のリストを分けた)

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -3,32 +3,40 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <body>
+<body>
 @foreach($user as $users)
     <!-- top3 -->
-     <div class="list-group" style="max-width: 400px; ">
-        <a class="list-group-item">1位</a>
-        <a class="list-group-item">名前:{{$users[0]->name}}</a>
-        <a class="list-group-item">ポイント：{{$users[0]->u_point}}</a>
-    </div>
-    <div>
-        <p>2位</p>
-        <p>名前:{{$users[1]->name}}</p>
-        <p>ポイント：{{$users[1]->u_point}}</p>
-    </div>
-        <p>3位</p>
-        <p>名前:{{$users[2]->name}}</p>
-        <p>ポイント：{{$users[2]->u_point}}</p>
-    </div>
-    @for($i = 3; $i <count($users); $i++)
-        <div class="list-group" style="max-width: 400px;">
-            <a class="list-group-item"> {{$i+1}}位 名前：{{$users[$i]->name}}  ポイント：{{$users[$i]->u_point}}</a>
+    <div class="position" Style="margin:200px;margin-bottom: 20px ">
+        <div class="list-group " style=" width: auto; max-width: 400px;--bs-list-group-color: #ffbc22">
+            <a class="list-group-item">1位</a>
+            <a class="list-group-item">名前:{{$users[0]->name}}</a>
+            <a class="list-group-item">ポイント：{{$users[0]->u_point}}</a>
         </div>
+        <div class="list-group " style="float:left;width: 180px; --bs-list-group-color: #6d95a7">
+            <a class="list-group-item">2位</a>
+            <a class="list-group-item">名前:{{$users[1]->name}}</a>
+            <a class="list-group-item">ポイント：{{$users[1]->u_point}}</a>
+        </div>
+
+        <div class="list-group" style="width:auto;--bs-list-group-color: #b17c00">
+
+            <a class="list-group-item">3位</a>
+            <a class="list-group-item">名前:{{$users[2]->name}}</a>
+            <a class="list-group-item">ポイント：{{$users[2]->u_point}}</a>
+        </div>
+    </div>
+    <div class="position-static" style="margin-left: 200px">
+        @for($i = 3; $i <count($users); $i++)
+
+            <div class="list-group" style="width:360px;">
+                <a class="list-group-item"> {{$i+1}}位 名前：{{$users[$i]->name}} ポイント：{{$users[$i]->u_point}}</a>
+            </div>
+
     @endfor
 @endforeach
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
 
