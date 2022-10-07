1922cae2c3818dd8aef37aee4ba0836efbc5458e
tsubasa <2001218@s.asojuku.ac.jp>
2022-10-07 10:33:39 +0900

    ヘッダー加えた

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -6,28 +6,44 @@
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
 <body>
+
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+            <span class="fs-4">ランキング</span>
+        </a>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active">ジャンル選択</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">マイページ</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+
+        </ul>
+    </header>
+</div>
 @foreach($user as $users)
     <!-- top3 -->
-    <div class="position" Style="margin:200px;margin-bottom: 20px ">
+    <div class="container-xxl" Style="margin:auto;margin-bottom: 20px ">
         <div class="list-group " style=" width: auto; max-width: 400px;--bs-list-group-color: #ffbc22">
-            <a class="list-group-item">1位</a>
+            <a class="list-group-item ">1位</a>
             <a class="list-group-item">名前:{{$users[0]->name}}</a>
             <a class="list-group-item">ポイント：{{$users[0]->u_point}}</a>
         </div>
-        <div class="list-group " style="float:left;width: 180px; --bs-list-group-color: #6d95a7">
+        <div class="list-group " style="float:left;width: 200px; --bs-list-group-color: #6d95a7">
             <a class="list-group-item">2位</a>
             <a class="list-group-item">名前:{{$users[1]->name}}</a>
-            <a class="list-group-item">ポイント：{{$users[1]->u_point}}</a>
+            <ag class="list-group-item">ポイント：{{$users[1]->u_point}}</ag>
         </div>
 
-        <div class="list-group" style="width:auto;--bs-list-group-color: #b17c00">
+        <div class="list-group" style="width:auto;max-width: 200px;--bs-list-group-color: #b17c00">
 
             <a class="list-group-item">3位</a>
             <a class="list-group-item">名前:{{$users[2]->name}}</a>
             <a class="list-group-item">ポイント：{{$users[2]->u_point}}</a>
         </div>
     </div>
-    <div class="position-static" style="margin-left: 200px">
+    <div class="container-md" style="margin:auto;margin-bottom: 20px">
         @for($i = 3; $i <count($users); $i++)
 
             <div class="list-group" style="width:360px;">
@@ -36,6 +52,7 @@
 
     @endfor
 @endforeach
+
 <script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
