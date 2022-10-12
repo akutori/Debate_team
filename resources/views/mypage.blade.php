f49bf7b016e3660dcbc75a217df2594105d2ae82
kuesu <2001160@s.asojuku.ac.jp>
2022-10-12 10:10:56 +0900

    ログアウト無機能ボタン

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -7,6 +7,7 @@
     <button type="button" onclick="location.href='{{url('/ranking')}}'">ランキングに行く</button>
     <button type="button" onclick="location.href='{{url('/delroom')}}'">ルーム削除</button>
     <button type="button" onclick="location.href='{{url('/readme')}}'">説明を見る</button>
+    <button type="button" >ログアウト</button>
 </body>
 </html>
 
