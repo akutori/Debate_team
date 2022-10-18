72d6c07e7d645f49efc5f68b7034a70ef258d91e
tsubasa <2001218@s.asojuku.ac.jp>
2022-10-18 21:34:59 +0900

    ログイン、アカウント登録ボタンの位置をタイトルの下に配置

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -16,24 +16,28 @@
     <div class="ht">
         <a href="{{url('/sgenre')}}" ><img class="rogo" src="{{asset('images/debate01.png')}}"></a>
     </div>
-    <div class="regi">
 
 
+<div class="p-5 mx-auto" >
         <!---ログイン,会員登録遷移--->
         @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+
+
                 @auth
-                    <button onclick="location.href='{{ url('/home') }}'" class=" text-sm text-gray-700 dark:text-gray-500 underline">Home</button>
+                    <button onclick="location.href='{{ url('/home') }}'" class=" ">Home</button>
                 @else
-                    <button onclick="location.href='{{ route('login') }}'" class="btn btn-primary text-sm text-gray-700 dark:text-gray-500 underline">ログイン</button>
+                    <button onclick="location.href='{{ route('login') }}'" class="btn btn-primary">ログイン</button>
 
                     @if (Route::has('register'))
-                        <button onclick="location.href='{{ route('register') }}'" class="btn btn-primary ml-4 text-sm text-gray-700 dark:text-gray-500 underline">アカウント登録</button>
+                        <button onclick="location.href='{{ route('register') }}'" class="btn btn-primary ">アカウント登録</button>
                     @endif
                 @endauth
-            </div>
+
+
         @endif
+
     </div>
+
     @yield('body')
 
         @yield('js')
