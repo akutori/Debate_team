2a10c107cfe58e02ba142343ddc59b16e4ba78d3
tsubasa <2001218@s.asojuku.ac.jp>
2022-10-24 15:01:57 +0900

    header.blade.phpのコメントアウト追記 ボタンの位置を中央に設置

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,5 +1,9 @@
 <html>
+<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 <body>
+<div class="grid text-center">
+    <a href="{{url('/sgenre')}}" ><img class="w-50 h-50" src="{{asset('images/debate01.png')}}"></a>
+</div>
     <h1>Mypage</h1>
     <p>{{$username}}さん</p>
     <p>
@@ -8,12 +12,22 @@
         </a>
     </p>
 
-    <button type="button" onclick="location.href='{{url('/sgenre')}}'">ジャンル選択</button>
-    <button type="button" onclick="location.href='{{url('/makeroom')}}'">ルーム作成</button>
-    <button type="button" onclick="location.href='{{url('/ranking')}}'">ランキングに行く</button>
-    <button type="button" onclick="location.href='{{url('/delroom')}}'">ルーム削除</button>
-    <button type="button" onclick="location.href='{{url('/readme')}}'">説明を見る</button>
-    <button type="button" >ログアウト</button>
+
+
+    <div class="col grid text-center h-25 ">
+
+    <button type="button" class="g-col-6 " onclick="location.href='{{url('/sgenre')}}'">ジャンル選択</button>
+    <button type="button" class="g-col-6" onclick="location.href='{{url('/makeroom')}}'">ルーム作成</button>
+    <button type="button" class="g-col-6" onclick="location.href='{{url('/ranking')}}'">ランキングに行く</button>
+    </div>
+
+    <div class="col grid text-center ">
+    <button type="button" class="g-col-6" onclick="location.href='{{url('/delroom')}}'">ルーム削除</button>
+    <button type="button" class="g-col-6" onclick="location.href='{{url('/readme')}}'">説明を見る</button>
+    <button type="button" class="g-col-6" >ログアウト</button>
+    </div>
+
+
 </body>
 </html>
 
