caa4c33fc79715267cfde58fb4e34fd0233096cb
宮里一樹 <2001204@s.asojuku.ac.jp>
2022-10-31 13:45:58 +0900

    mypage画面のレイアウト完成

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -21,19 +21,19 @@
     <div class="row row-cols-1 row-cols-lg-2 justify-content-center">
         <div class="card m-2" style="width: 15rem;">
             <a href="{{ url('/sgenre') }}" class="delay-time fadeUp ">
-                <img class="card-img-top p-2 rounded-circle" src="{{asset('./images/irast2.jpg')}}">
+                <img class="card-img-top p-2 rounded-circle" src="{{asset('./images/口論余白改良形.png')}}">
             </a>
-            <button type="button" class="btn rounded-pill btn-primary mb-2" onclick="location.href='{{url('/sgenre')}}'">ジャンル選択</button>
+            <button type="button" class="btn rounded-pill btn-primary mb-2" onclick="location.href='{{url('/sgenre')}}'">ディベート</button>
         </div>
         <div class="card m-2" style="width: 15rem;">
             <a href="{{ url('/makeroom') }}">
-                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/irast2.jpg') }}">
+                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/ルーム作成.png') }}">
             </a>
             <button type="button" class="btn rounded-pill btn-primary mb-2" onclick="location.href='{{url('/makeroom')}}'">ルーム作成</button>
         </div>
         <div class="card m-2" style="width: 15rem;">
             <a href="{{ url('/ranking') }}">
-                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/irast2.jpg') }}">
+                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/ランキング余白改良形.png') }}">
             </a>
             <button type="button" class="btn rounded-pill btn-primary mb-2" onclick="location.href='{{url('/ranking')}}'">ランキングに行く</button>
         </div>
@@ -42,19 +42,19 @@
     <div class="row row-cols-1 row-cols-lg-2 justify-content-center">
         <div class="card m-2" style="width: 15rem;">
             <a href="{{ url('/delroom') }}">
-                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/irast2.jpg') }}">
+                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/削除余白改良形.png') }}">
             </a>
             <button type="button" class="btn rounded-pill btn-primary mb-2" onclick="location.href='{{url('/delroom')}}'">ルーム削除</button>
         </div>
         <div class="card m-2" style="width: 15rem;">
             <a href="{{ url('/') }}">
-                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/irast2.jpg') }}">
+                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/説明余白改良形.png') }}">
             </a>
             <button type="button" class="btn rounded-pill btn-primary mb-2" onclick="location.href='{{url('/')}}'">説明を見る</button>
         </div>
         <div class="card m-2" style="width: 15rem;">
             <a href="{{ url('/') }}">
-                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/irast2.jpg') }}">
+                <img class="card-img-top p-2 rounded-circle" src="{{ asset('./images/ログアウト余白改良形.png') }}">
             </a>
             <button type="button" class="btn btn-danger mb-2  rounded-pill" >ログアウト</button>
         </div>
