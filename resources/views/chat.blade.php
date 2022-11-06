7cb36928964cc207bd934f26556bb8b3d8147167
akutori <78586241+akutori@users.noreply.github.com>
2022-11-06 20:43:39 +0900

    テンプレートのコメントアウトしていた箇所を削除

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -37,40 +37,6 @@ use Illuminate\Support\Facades\Auth;
                 {{-- チャット履歴を表示させる --}}
             </div>
         </div>
-        {{-- modal_start --}}
-        {{--
-        <!-- 切り替えボタンの設定 -->
-        <div class="container">
-            <div class="row">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#lockalert" id="openmodal">
-                    ココを押すとアラートによるチャットのロックデモを表示
-                </button>
-            </div>
-        </div>
-        <!-- モーダルの設定 -->
-        <div class="modal fade" id="lockalert" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content bg-danger">
-                    <div class="modal-header text-light">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-exclamation-octagon-fill" viewBox="0 0 16 16">
-                            <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                        </svg>
-                        <h1 class="modal-title fs-2 ps-3" id="staticBackdropLabel">攻撃的なコメントを検知しました!!</h1>
-                    </div>
-                    <div class="modal-body">
-                        <p class="fs-1 text-light">警告</p>
-                        <p class="text-light fs-4">10秒間コメントをロックします</p>
-                    </div>
-                    <div class="modal-footer">
-                        <div class="mx-auto d-block">
-                            <button type="button" class="btn btn-warning btn-lg" data-bs-dismiss="modal" id="modalbutton">閉じる</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        --}}
-        {{-- modal_end --}}
     @if($state==0)
         {{---  チャット送信  ---}}
         {{--- web.phpの/chat ---}}
