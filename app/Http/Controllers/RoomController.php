43a09734d4cbb783efa2c9478389624934f164b2
akutori <78586241+akutori@users.noreply.github.com>
2022-11-12 09:42:48 +0900

    select_user_roomのコンフリクトを解消

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -13,12 +13,8 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Exception;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
-use Illuminate\Session\TokenMismatchException;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
