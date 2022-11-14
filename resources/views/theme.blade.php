87efd837fe1e66f65abf01a6463dc1e62cc01512
Dev3rd <rikubow123@gmail.com>
2022-11-14 14:52:06 +0900

    テーマ選択をモーダルに置き換え(完成！) モーダル周りの不具合を修正中(完成！)

--- resources/views/theme.blade.php
+++ resources/views/theme.blade.php
@@ -5,7 +5,7 @@
     <script src="{{asset('/js/app.js')}}"></script>
     <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus:ital@1&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300&display=swap" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="{{asset('css/theme.css')}}">-->
+   <link rel="stylesheet" href="{{asset('css/theme.css')}}">
     <link rel="stylesheet" href="{{asset('js/theme.js')}}">
 
 @endsection
@@ -43,38 +43,19 @@
 
 
             <!-- Modal -->
-            <div class="modal fade" id="dialog{{$room_once->t_id}}" tabindex="-1" aria-labelledby="exampleModalLabel"
-                 aria-hidden="true">
-                <div class="modal-dialog  modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <p class="p1" id="exampleModalLabel">参加しますか？</p>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <p class="p2">{{$room_once->t_name}}</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button class="btn btn-outline-info btn-secondary border-secondary text-white " data-bs-toggle="modal" data-bs-target="#dialog2{{$room_once->t_id}}"
-                                    onclick="document.getElementById('dialog2{{$room_once->t_id}}').show();">参加する
-                            </button>
-                            <button class="btn btn-outline-danger btn-secondary border-secondary text-white " data-bs-dismiss="modal">戻る</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="modal fade" id="dialog2{{$room_once->t_id}}" tabindex="-1" aria-labelledby="exampleModalLabels"
-                 aria-hidden="true">
+
+            <div class="modal fade" id="dialog{{$room_once->t_id}}" tabindex="-1" aria-labelledby="exampleModalLabels"
+                 aria-hidden="true" >
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <p class="p1" id="exampleModalLabels">Choose Position</p>
+                            <span class="fs-3" id="exampleModalLabels">Choose Position</span>
 
                         </div>
                         <div class="modal-body">
-                            <p class="p3">Choose Position</p>
-                            <p class="p4">あなたの立場を選んでください</p>
+                            <p class="p2">{{$room_once->t_name}}</p>
+
                             <p class="p5">※傍観者の中からランダムに投票権が付与されます。</p>
                         </div>
                         <div class="modal-footer">
@@ -85,30 +66,64 @@
                             <input type="button" class="btn btn-outline-info btn-secondary border-secondary text-white "
                                    onclick="location.href='{{url('/standby/'.$room_once->r_id.'/'.$bystander_flag)}}'"
                                    value="傍観者">
-                            <button class="btn btn-outline-danger btn-secondary border-secondary text-white " data-bs-target="#dialog{{$room_once->t_id}}" data-bs-dismiss="modal">戻る</button>
+                            <button class="btn btn-outline-danger btn-secondary border-secondary text-white "   data-bs-dismiss="modal">戻る</button>
                         </div>
                     </div>
                 </div>
             </div>
             {{-- ここに一覧を表示させる --}}
 
-        <!-- <div id="dialog2{{$room_once->r_id}}" class="modal-dialog "  >
-                        <div class="modal-body">
-                            <p class="p3">Choose Position</p>
-                            <p class="p4">あなたの立場を選んでください</p>
-                            <p class="p5">※傍観者の中からランダムに投票権が付与されます。</p>
-                        </div>
-                        <div class="modal-footer">
-                                {{-- 部屋IDとユーザーIDは前の画面から取得して状態のみこちらから指定する --}}
-            <input type="button" class="popb1" onclick="location.href='{{url('/standby/'.$room_once->r_id.'/'.$debater_flag)}}'" value="発言者">
-                                <input type="button" class="popb1" onclick="location.href='{{url('/standby/'.$room_once->r_id.'/'.$bystander_flag)}}'" value="傍観者">
-                        </div>
+        <!--
+            <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalToggleLabel">Modal 1</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Show a second modal and hide this one with the button below.
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Open second modal</button>
+      </div>
+    </div>
+  </div>
+</div>
 
-                            <button class="popb1" onclick="document.getElementById('dialog2{{$room_once->t_id}}').close();">戻る</button>-->
+<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Hide this modal and show the first with the button below.
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" data-bs-dismiss="modal">Back to first</button>
+      </div>
+    </div>
+  </div>
+</div>
+<a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Open first modal</a>
 
-            <!-- <form action="" method="post">
-                <button class="popb2" type="submit" name="out" value="1">はい</button>
-            </form> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            -->
 
 
 
@@ -119,6 +134,6 @@
     </div>
     </div>
     </div>
-    <!-- <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-    <script src={{asset('js/theme.js')}}></script>-->
+   <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+    <script src={{asset('js/theme.js')}}></script>
 @endsection
