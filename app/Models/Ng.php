88a5dfc544a6db025c9b17254a270880997d193a
宮里一樹 <2001204@s.asojuku.ac.jp>
2022-11-21 11:15:57 +0900

    ngword編集画面（管理者用）→登録されているデータの一覧表示、新規登録の機能完成

--- app/Models/Ng.php
+++ app/Models/Ng.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Ng extends Model
 {
     use HasFactory;
     protected $fillable = ["n_words"];
+    protected $primaryKey='n_id';
+    public $timestamps = false;
+
+    // ngテーブルの全件取得
+    public function ngList(){
+        return DB::table("ngs")->get();
+    }
+
+    // ngテーブルにキーワードを新規登録
+    public function ngwordInsert($n_words){
+        $ng = new NG();
+        $ng->n_id = null;
+        $ng->n_words = $n_words;
+       //\DB::table('ngs')->insert(['n_words' => $n_words]);
+       $ng->save();
+    }
 }
--- resources/views/ngword.blade.php
+++ resources/views/ngword.blade.php
@@ -1,1 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <script src="{{asset('js/app.js')}}"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
 <h1>NGワード画面</h1>
+
+<!-- NGwordを登録する用のボタン -->
+<button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#ngwordModal">
+    追加登録
+</button>
+
+<table>
+    <tr>
+        <td>キーワード</td>
+    </tr>
+    @foreach( $list as $ng )
+        <tr>
+            <th>{{ $ng -> n_words }}</th>
+        </tr>
+    @endforeach
+</table>
+
+<!-- NGwordを登録するモーダルを表示 -->
+<div class="modal fade" id="ngwordModal" tabindex="-1" aria-labelledby="ngwordTitle" aria-hidden="true" data-bs-backdrop="static">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="{{ url('/ngwordEdit/insert') }}" method="get">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ngwordTitle">NGword 新規登録♪</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    登録したいキーワードを入力してください
+                    <input type="text" class="form-control" name="ngword">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">戻る</button>
+                    <button type="submit" class="btn btn-outline-primary">登録</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>
+    var ngwordModal = document.getElementById('ngwordModal')
+    ngwordModal.addEventListener('click', function (event) {
+        // Button that triggered the modal
+        var button = event.relatedTarget
+        // Extract info from data-bs-* attributes
+        var recipient = button.getAttribute('data-bs-whatever')
+        // If necessary, you could initiate an AJAX request here
+        // and then do the updating in a callback.
+        //
+        // Update the modal's content.
+        var modalTitle = exampleModal.querySelector('.modal-title')
+        var modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+        modalTitle.textContent = 'New message to ' + recipient
+        modalBodyInput.value = recipient
+    })
+</script>
+</body>
+</html>
+
