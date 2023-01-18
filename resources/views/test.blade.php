c2cf3f92588c818e9bd69dfccdfc126039088dfc
akutori <78586241+akutori@users.noreply.github.com>
2023-01-18 11:56:25 +0900

    ログイン画面がスマートフォンで小さかかった問題を修正

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{asset('css/test.css')}}">
     <title>{{config('app.name')}}</title>
     <!-- Scripts -->
